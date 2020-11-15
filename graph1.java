82538ba91d6e7a3153939f043719609a38caf8f4
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-11-15 11:07:07 +0000

    test oh1 and oh2

--- graph1.java
+++ graph1.java
@@ -27,6 +27,20 @@ public class graph1 {
     }
   }
 
+  static void printRoomSquare(int[][][] R) {
+    for(int i = 1; i < R.length; i++) {
+      System.out.print("<");
+        for(int j = 1; j < R[i].length; j++) {
+            System.out.print("<");
+            System.out.print(((R[i][j][0] < 0)?"-":String.valueOf(R[i][j][0])));
+            System.out.print(",");
+            System.out.print(((R[i][j][1] < 0)?"-":String.valueOf(R[i][j][1])));
+            System.out.print(">");
+        }
+        System.out.println(">");
+    }
+  }
+
   // true if vertex v has incident uncoloured edge
   static boolean liveVertex(int[][] g, int v, int n, int r) {
     int c; boolean result; int count;
@@ -172,6 +186,77 @@ public class graph1 {
     return count;
   }
 
+  static void oh1(int[][] g1, int[][] g2, int[][][] R, int n, int r) {
+    int u, v, w, c1j, c1k, c2;
+    do {
+      u = random(0, r - 1);
+    }
+    while (!liveVertex(g2, u, n, r));
+    do {
+      c2 = random(1, n - 1);
+    } while (!(liveColour(g2, c2, r) & !colouredWith(g2, c2, u, r)));
+    do {
+      v = random(0, r - 1);
+    } while (! (u != v & !edgeColoured(g2, u, v, n)));
+
+    c1j = colourOf(g1, u, v, n);
+
+    if (R[c1j][c2][0] != -1) return;
+
+    if (! colouredWith(g2, c2, v, r)) {
+        g2[c2][u] = v;
+        g2[c2][v] = u;
+        R[c1j][c2][0] = u;
+        R[c1j][c2][1] = v;
+    }
+    else {
+        w = g2[c2][v];
+        g2[c2][v] = -1;
+        g2[c2][w] = -1;
+        g2[c2][u] = v;
+        g2[c2][v] = u;
+        R[c1j][c2][0] = u;
+        R[c1j][c2][1] = v;
+        c1k = colourOf(g1, w, v, n);
+        R[c1k][c2][0] = -1;
+        R[c1k][c2][1] = -1;
+    }
+  }
+
+  static void oh2(int[][] g1, int[][] g2, int[][][] R, int n, int r) {
+    int u,v,c1j,c2i,c2k;
+    do {
+      c2i = random(1, n - 1);
+    } while (!liveColour(g2, c2i, r));
+    do {
+      u = random(0, r - 1);
+      v = random(0, r - 1);
+    }
+    while (!(u != v & !colouredWith(g2, c2i, u, r) & !colouredWith(g2, c2i, v, r)));
+
+    c1j = colourOf(g1, u, v, n);
+
+    if (R[c1j][c2i][0] != -1) return;
+
+    if (! edgeColoured(g2, u, v, n)) {
+      g2[c2i][u] = v;
+      g2[c2i][v] = u;
+      R[c1j][c2i][0] = u;
+      R[c1j][c2i][1] = v;
+    }
+    else {
+      c2k = colourOf(g2, u, v, n);
+      g2[c2k][u] = -1;
+      g2[c2k][v] = -1;
+      g2[c2i][u] = v;
+      g2[c2i][v] = u;
+      R[c1j][c2i][0] = u;
+      R[c1j][c2i][1] = v;
+      R[c1j][c2k][0] = -1;
+      R[c1j][c2k][1] = -1;
+    }
+  }
+
   //
   static void test_initGraph() {
     int[][] graph1 = new int[3][2];
@@ -197,9 +282,39 @@ public class graph1 {
     printGraph(graph1);
   }
 
+  static void test_oh1() {
+    int n = 4;
+    int r = 4;
+    int[][] graph1 = new int[n][n];
+    int[][] graph2 = new int[n][n];
+    int[][][] R = new int[n][n][2];
+    initGraph(graph1);
+    oneFactorisation(graph1, n, r);
+    initGraph(graph2);
+    initRoomSquare(R);
+    oh1(graph1, graph2, R, n, r);
+    printRoomSquare(R);
+  }
+
+  static void test_oh2() {
+    int n = 4;
+    int r = 4;
+    int[][] graph1 = new int[n][n];
+    int[][] graph2 = new int[n][n];
+    int[][][] R = new int[n][n][2];
+    initGraph(graph1);
+    oneFactorisation(graph1, n, r);
+    initGraph(graph2);
+    initRoomSquare(R);
+    oh2(graph1, graph2, R, n, r);
+    printRoomSquare(R);
+  }
+
   public static void main (String[] args) {
     //test_initGraph();
     //test_h1();
-    test_oneFactorisation();
+    //test_oneFactorisation();
+    //test_oh1();
+    test_oh2();
   }
 }
