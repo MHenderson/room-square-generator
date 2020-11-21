5131c722c7fd7fee0ba7ddaa79805974e3d8ab83
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-11-21 16:51:37 +0000

    fix seed issue #3

--- Room/algorithm.java
+++ Room/algorithm.java
@@ -7,18 +7,18 @@ public class algorithm {
     return(value);
   }
 
-  static void h1(int[][] g) {
+  static void h1(int[][] g, java.util.Random rng) {
     int u, v, c, w;
     int n = g.length;
     int r = g[0].length;
     do {
-      u = random(0, r - 1);
+      u = rng.nextInt(r);
     } while (!graph.liveVertex(g, u));
     do {
-      c = random(1, n - 1);
+      c = rng.nextInt(n - 1) + 1;
     } while (!(graph.liveColour(g, c) & !graph.colouredWith(g, c, u)));
     do {
-      v = random(0, r - 1);
+      v = rng.nextInt(r);
     } while (!(u != v & !graph.edgeColoured(g, u, v)));
     if (!graph.colouredWith(g, c, v)) {
       g[c][u] = v;
@@ -33,16 +33,16 @@ public class algorithm {
     }
   }
 
-  static void h2(int[][] g) {
+  static void h2(int[][] g, java.util.Random rng) {
     int u, v, c, d;
     int n = g.length;
     int r = g[0].length;
     do {
-      c = random(1, n - 1);
+      c = rng.nextInt(n - 1) + 1;
     } while (!graph.liveColour(g, c));
     do {
-      u = random(0, r - 1);
-      v = random(0, r - 1);
+      u = rng.nextInt(r);
+      v = rng.nextInt(r);
     } while (!(u != v & !graph.colouredWith(g, c, u) & !graph.colouredWith(g, c, v)));
     if (!graph.edgeColoured(g, u, v)) {
       g[c][u] = v;
@@ -57,18 +57,18 @@ public class algorithm {
     }
   }
 
-  static void oh1(int[][] g1, int[][] g2, int[][][] R) {
+  static void oh1(int[][] g1, int[][] g2, int[][][] R, java.util.Random rng) {
     int u, v, w, c1j, c1k, c2;
     int n = g1.length;
     int r = g1[0].length;
     do {
-      u = random(0, r - 1);
+      u = rng.nextInt(r);;
     } while (!graph.liveVertex(g2, u));
     do {
-      c2 = random(1, n - 1);
+      c2 = rng.nextInt(n - 1) + 1;
     } while (!(graph.liveColour(g2, c2) & !graph.colouredWith(g2, c2, u)));
     do {
-      v = random(0, r - 1);
+      v = rng.nextInt(r);
     } while (! (u != v & !graph.edgeColoured(g2, u, v)));
 
     c1j = graph.colourOf(g1, u, v);
@@ -95,16 +95,16 @@ public class algorithm {
     }
   }
 
-  static void oh2(int[][] g1, int[][] g2, int[][][] R) {
+  static void oh2(int[][] g1, int[][] g2, int[][][] R, java.util.Random rng) {
     int u, v, c1j, c2i, c2k;
     int n = g1.length;
     int r = g1[0].length;
     do {
-      c2i = random(1, n - 1);
+      c2i = rng.nextInt(n - 1) + 1;
     } while (!graph.liveColour(g2, c2i));
     do {
-      u = random(0, r - 1);
-      v = random(0, r - 1);
+      u = rng.nextInt(r);
+      v = rng.nextInt(r);
     }
     while (!(u != v & !graph.colouredWith(g2, c2i, u) & !graph.colouredWith(g2, c2i, v)));
 
@@ -138,9 +138,9 @@ public class algorithm {
     do {
       int rr = rng.nextInt(2);
       if (rr == 0)
-        h1(g);
+        h1(g, rng);
       else
-        h2(g);
+        h2(g, rng);
       count++;
     } while (!graph.isFull(g));
     return count;
@@ -150,9 +150,9 @@ public class algorithm {
     for (int j = 0; j < iterations; j++) {
       int rr = rng.nextInt(2);
       if (rr == 0) {
-        oh1(g, h, R);
+        oh1(g, h, R, rng);
       } else {
-        oh2(g, h, R);
+        oh2(g, h, R, rng);
       }
       if (graph.isFull(h)) {
         System.err.println();
