519467b3aa485a25ea194ecd10814278541f5e2e
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-02-21 18:01:03 +0000

    update package name in source

--- src/test/java/org/Room/graphTest.java
+++ src/test/java/org/Room/graphTest.java
@@ -8,7 +8,7 @@ public class graphTest extends TestCase {
 
     public void testN() {
         Random randnum = new Random(42);
-        Room.graph g = new Room.graph(3, 3, randnum);
+        graph g = new graph(3, 3, randnum);
         assertEquals(g.n(), 3);
     }
 }
