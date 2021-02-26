76a8a09ff514db61d77d5e660b3d110232d258a1
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-02-26 10:34:51 +0000

    add prompts to the main script

--- main.sh
+++ main.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-java -Dfile.encoding=UTF-8 -jar out/artifacts/room_square_generator_jar/room-square-generator.jar 12 400 30 42
+read -p "Size [12]: " size
+size=${size:-12}
+read -p "Iterations [400]: " its
+its=${its:-400}
+read -p "Restarts [30]: " restarts
+restarts=${restarts:-30}
+read -p "Random seed [42]: " seed
+seed=${seed:-42}
+java -Dfile.encoding=UTF-8 -jar out/artifacts/room_square_generator_jar/room-square-generator.jar $size $its $restarts $seed
 
